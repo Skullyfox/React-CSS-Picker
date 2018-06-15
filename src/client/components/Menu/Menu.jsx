11a454a899f5df9e54fb985be419646c497ffa88
Skullyfox <skullyfox.yt@gmail.com>
2018-06-16 01:54:42 +0200

    Menu style update + dropdown first category

--- src/client/components/Menu/Menu.jsx
+++ src/client/components/Menu/Menu.jsx
@@ -4,6 +4,26 @@ import template      from './template.html.js';
 class Menu extends React.Component{
     constructor (props) {
         super(props);
+        this.state = {
+            toggled : true,
+        }
+    }
+
+    droppedToggle(){
+        this.setState({ toggled : !this.state.toggled })
+
+
+        /* let menu = document.querySelector('.dropDownCategory');
+
+        if(!this.state.toggled){
+            setTimeout(
+                () => {
+                    menu.style='display : none;'
+                },
+            800);
+        } else {
+            menu.style='display : block;'
+        } */
     }
 
     render(){
--- src/client/components/Menu/template.html.js
+++ src/client/components/Menu/template.html.js
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 export default function () {
-    return React.createElement('div', { 'className': 'menu' }, React.createElement('p', {}, 'sdsqd'));
+    return React.createElement('div', { 'className': 'menu' }, React.createElement('h1', { 'className': 'menuTitle' }, 'Menu'), React.createElement('div', { 'className': 'separator' }), React.createElement('button', {
+        'className': 'dropDownToggler',
+        'onClick': () => this.droppedToggle()
+    }, 'Category #1'), React.createElement('div', { 'className': 'dropDownCategory ' + (this.state.toggled ? 'hidden' : '') }, React.createElement('li', {}, React.createElement('p', {}, 'Keyframes Picker')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]'))), React.createElement('button', { 'className': 'dropDownToggler' }, 'Category #2'));
 }
