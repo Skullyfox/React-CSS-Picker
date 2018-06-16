b16c6b1d5b6c4412c9081c576d5c6b9f3f720ec3
Skullyfox <skullyfox.yt@gmail.com>
2018-06-16 02:06:35 +0200

    replace 'Category #2' by '[Coming Soon]'

--- src/client/components/Menu/template.html.js
+++ src/client/components/Menu/template.html.js
@@ -4,5 +4,5 @@ export default function () {
     return React.createElement('div', { 'className': 'menu' }, React.createElement('h1', { 'className': 'menuTitle' }, 'Menu'), React.createElement('div', { 'className': 'separator' }), React.createElement('button', {
         'className': 'dropDownToggler',
         'onClick': () => this.droppedToggle()
-    }, 'Category #1'), React.createElement('div', { 'className': 'dropDownCategory ' + (this.state.toggled ? 'hidden' : '') }, React.createElement('li', {}, React.createElement('p', {}, 'Keyframes Picker')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]'))), React.createElement('button', { 'className': 'dropDownToggler' }, 'Category #2'));
+    }, 'Category #1'), React.createElement('div', { 'className': 'dropDownCategory ' + (this.state.toggled ? 'hidden' : '') }, React.createElement('li', {}, React.createElement('p', {}, 'Keyframes Picker')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]'))), React.createElement('button', { 'className': 'dropDownToggler' }, '[Coming Soon]'));
 }
