84e677151b846efe6225c478a7b291d611e475d6
Skullyfox <skullyfox.yt@gmail.com>
2018-06-19 13:18:01 +0200

    Partial routing added

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,23 +1,50 @@
 import React                              from 'react';
 import { BrowserRouter, Switch, Route}    from 'react-router-dom';
 
+
 import Home                               from './pages/Home/Home.jsx';
 import Menu                               from './components/Menu/Menu.jsx';
 import Loader                          from './pages/Loader/Loader.jsx';
 
 class App extends React.Component {
+
     constructor(props) {
         super(props);
+        this.state={
+            homeView: true,
+            loaderView: false,
+        }
+        this.goLoaderView = this.goLoaderView.bind(this);
+        this.goMenuView = this.goMenuView.bind(this);
+    }
+
+    goLoaderView(){
+        this.setState({
+            loaderView : true,
+            homeView: false
+        })
+    }
+
+    goMenuView(){
+        this.setState({
+            loaderView : false,
+            homeView: true
+        })
     }
 
     render() {
         return (
             <BrowserRouter>
                 <React.Fragment>
-                    <Menu />
+                    <Menu 
+                        onClick={this.goLoaderView} 
+                        onClick2={this.goMenuView} 
+                        />
                     <Switch>
-                        <Route path="/" component={Loader}/>
-                        <Route path="/Loader" component={Loader}/>
+                        {this.state.homeView ? (<Home />): null}
+                        {this.state.loaderView ? (<Loader />): null}
+                        {/* <Route path="/" component={Loader}/>
+                        <Route path="/Loader" component={Loader}/> */}
                     </Switch>
                 </React.Fragment>    
             </BrowserRouter>
--- src/client/components/Menu/template.html.js
+++ src/client/components/Menu/template.html.js
@@ -3,12 +3,13 @@ import * as _ from 'lodash';
 export default function () {
     return React.createElement('div', { 'className': 'menu' }, React.createElement('h1', { 'className': 'menuTitle' }, 'Menu'), React.createElement('div', { 'className': 'separator' }), React.createElement('button', {
         'id': 'homeButton',
-        'className': 'dropDownToggler'
+        'className': 'dropDownToggler',
+        'onClick': this.props.onClick2
     }, 'Home'), React.createElement('button', {
         'id': 'firstCategoryButton',
         'className': 'dropDownToggler',
         'onClick': () => this.droppedToggle()
-    }, 'Keyframes'), React.createElement('div', { 'className': 'dropDownCategory ' + (this.state.toggled ? 'hidden' : '') }, React.createElement('li', {}, React.createElement('p', {}, 'Loader Picker')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]'))), React.createElement('button', {
+    }, 'Keyframes'), React.createElement('div', { 'className': 'dropDownCategory ' + (this.state.toggled ? 'hidden' : '') }, React.createElement('li', { 'onClick': this.props.onClick }, React.createElement('p', {}, 'Loader Picker')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]')), React.createElement('li', {}, React.createElement('p', {}, '[Coming Soon]'))), React.createElement('button', {
         'id': 'secondCategoryButton',
         'className': 'dropDownToggler'
     }, '[Coming Soon]'));
