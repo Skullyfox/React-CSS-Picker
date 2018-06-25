5a49d25a140fedda3e0c4840a50f27294d5beab4
Skullyfox <Skullyfox.yt@gmail.com>
2018-06-25 22:41:34 +0200

    keyframes render method changed

--- src/client/components/keyframesList/keyframesList.jsx
+++ src/client/components/keyframesList/keyframesList.jsx
@@ -9,6 +9,7 @@ export default class KeyframesList extends Component {
     dimmer: 'blurring',
     modalOpen: false,
     keyframes: {
+        /* CIRCLE */
         "CircleOne": {
             "class" : "circle one",
             "title" : "Circle One",
@@ -83,10 +84,172 @@ export default class KeyframesList extends Component {
                 width: 50px 
                 border-radius: 100%
                 animation: circleLoad 1s infinite linear
-                border: 3px double $primary
+                border: 5px double $primary
                 border-bottom-color: $black
 
             @keyframes circleLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        /* SQUARE */
+        "SquareOne": {
+            "class" : "square one",
+            "title" : "Square One",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 3px solid $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "SquareTwo": {
+            "class" : "square two",
+            "title" : "Square Two",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 5px dotted $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "SquareThree": {
+            "class" : "square three",
+            "title" : "Square Three",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 3px dashed $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "SquareFour": {
+            "class" : "square four",
+            "title" : "Square Four",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 5px double $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        /* SQUARE */
+        "DNA Loader": {
+            "class" : "fas fa-dna",
+            "title" : "DNA Loader",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 3px solid $primary
+
+            @keyframes squareLoad 
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "Recycle Load": {
+            "class" : "fas fa-recycle",
+            "title" : "Recycle Load",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 5px dotted $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "Sync Load": {
+            "class" : "fas fa-sync-alt",
+            "title" : "Sync Load",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 3px dashed $primary
+
+            @keyframes squareLoad
+                0%
+                    transform: rotate(0deg)
+                100%
+                    transform: rotate(360deg)
+            `
+        },
+        "Hourglass Load": {
+            "class" : "fas fa-hourglass-start",
+            "title" : "Hourglass Load",
+            "code"  : `
+            /* Sass */
+
+            .square
+                height: 50px
+                width: 50px 
+                border-radius: 10px
+                animation: squareLoad 1s infinite linear
+                border: 5px double $primary
+
+            @keyframes squareLoad
                 0%
                     transform: rotate(0deg)
                 100%
